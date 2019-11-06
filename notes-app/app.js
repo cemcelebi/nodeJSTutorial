b180844e83bf855330f21a59d9506146df350f75
cemcelebi <cem.celebi.paul@gmail.com>
2019-11-06 14:22:58 +0300

    first app with Chalk!

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,14 @@
-const add = require('./utils.js')
-const sum = add(4,-2)
-console.log(sum)
+ const randomWithRange = require('./utils.js')
+ const chalk=require('./node_modules/chalk')
+// const sum = add(4,-2)
+// console.log(sum)
+//console.log(randomWithRange(1,5))
+const myNumber=randomWithRange(1,10)
+const log = console.log
+if (myNumber<=3) {
+    log(chalk.red(myNumber))
+  } else if (myNumber>3 && myNumber<=6) {
+    log(chalk.yellow(myNumber))
+  } else {
+    log(chalk.green(myNumber))
+}
--- notes-app/utils.js
+++ notes-app/utils.js
@@ -1,6 +1,10 @@
 console.log('utils.js')
 
-const add = function (a,b){
-    return a+b
-}
-module.exports = add
+// const add = function (a,b){
+//     return a+b
+// }
+// module.exports = add
+const randomWithRange=function (low, high) {
+    return Math.random() * (high - low) + low
+  }
+ module.exports=randomWithRange
